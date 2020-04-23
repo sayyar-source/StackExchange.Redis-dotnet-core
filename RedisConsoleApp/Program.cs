433d941ff54186ac6972d73c37d3a027c1ea1e44
eng.sayyar <eng.sayyar@gmail.com>
2020-04-23 20:13:24 +0300

    Add project files.

---
+++ RedisConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace RedisConsoleApp
+{
+    class Program
+    {
+        static void  Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost:6379");
+          IDatabase db=  redis.GetDatabase();
+           //string
+            db.StringSet("time", DateTime.Now.ToShortTimeString());
+            db.StringSet("city", "ankara");
+           // db.KeyDelete("city");
+            Console.WriteLine(db.StringGet("time")+"\n"+db.StringGet("city"));
+         
+            Console.ReadKey();
+        }
+    }
+}
