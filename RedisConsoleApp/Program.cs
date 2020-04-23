52bd36a5b096aa134c99e5c34b8bec53e5b81238
eng.sayyar <eng.sayyar@gmail.com>
2020-04-24 01:16:38 +0300

    hash redis

--- RedisConsoleApp/Program.cs
+++ RedisConsoleApp/Program.cs
@@ -5,17 +5,65 @@ namespace RedisConsoleApp
 {
     class Program
     {
-        static void  Main(string[] args)
+        static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost:6379");
-          IDatabase db=  redis.GetDatabase();
-           //string
-            db.StringSet("time", DateTime.Now.ToShortTimeString());
-            db.StringSet("city", "ankara");
-           // db.KeyDelete("city");
-            Console.WriteLine(db.StringGet("time")+"\n"+db.StringGet("city"));
-         
+            IDatabase db = redis.GetDatabase();
+            var hashkey = "user";
+            HashEntry[] userHashEntry =
+                {
+                new HashEntry("username","ali"),
+                new HashEntry("fullname","ali aydogan"),
+                new HashEntry("mobile","095545367"),
+                new HashEntry("address","ankara")
+                };
+            db.HashSet(hashkey, userHashEntry);
+            //get value
+            if (db.HashExists(hashkey, "username"))
+            {
+                var user = db.HashGet(hashkey, "username");
+                Console.WriteLine("username is: " + user);
+
+            }
+            var allHash = db.HashGetAll(hashkey);
+            //get all the items
+            foreach (var item in allHash)
+            {
+                //Console.WriteLine(item.Name+" "+ item.Value);
+                Console.WriteLine(string.Format("key : {0}, value : {1}", item.Name, item.Value));
+                // Result:
+                //key: name, value: hassan
+                //key : age, value: 26
+                //key: city, value: bonab
+                //key : username, value: ali
+                //key : fullname, value: ali aydogan
+                //key: mobile, value: 095545367
+                //key: address, value: ankara
+            }
+            //get all the values
+            var values = db.HashValues(hashkey);
+            foreach (var item in values)
+            {
+                Console.WriteLine(string.Format("value:{0}", item));
+            }
+            //get all the keys
+            var keys = db.HashKeys(hashkey);
+
+            foreach (var k in keys)
+            {
+                Console.WriteLine(k); 
+            }
+            //HashLength
+            var len = db.HashLength(hashkey);  
+            Console.WriteLine(len);
+
+            if (db.HashExists(hashkey, "age"))
+            {
+                var age = db.HashIncrement(hashkey, "age", 2); //age now becomes 28
+                Console.WriteLine(age);
+      
+            }
             Console.ReadKey();
         }
     }
